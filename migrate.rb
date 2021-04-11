8124adee09af6026670ac4a0024ac03f68b8fdbd
garigari-kun <keisuke.cs@gmail.com>
2021-04-11 16:50:53 +0900

    rename zsh_history to .zsh_history

--- migrate.rb
+++ migrate.rb
@@ -3,7 +3,7 @@ require 'yaml'
 File.open("fish_history", mode = "rt") do |f|
   f.each_line do |l|
     if l =~ /- cmd: /
-      File.open("zsh_history", 'a') {|f| f.write(l[7..-1]) }
+      File.open(".zsh_history", 'a') {|f| f.write(l[7..-1]) }
     end
   end
 end
